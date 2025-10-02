a55a8705b90a732fc7c6052be0a4ce3316d195ee
Nishida Keito <nsdkit0224@gmail.com>
2025-10-02 16:04:15 +0900

    Update OneSignalSDKUpdaterWorker.js

--- public/OneSignalSDKUpdaterWorker.js
+++ public/OneSignalSDKUpdaterWorker.js
@@ -1,1 +1,1 @@
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKUpdaterWorker.js?appId=66b12ad6-dbe7-498f-9eb6-f9d8031fa8a1');
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKUpdaterWorker.js');
